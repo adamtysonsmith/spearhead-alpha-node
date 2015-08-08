83fc6870fb3a6d5621bed3cc25f87a674f21d40a
adamtysonsmith <atsmith227@gmail.com>
2015-08-07 22:56:20 -0600

    added quick add UI to dashboard

--- public/js/materialize-nav.js
+++ public/js/materialize-nav.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     // Initialize Materialize Mobile side nav and modals
     $(".button-collapse").sideNav();
     $('.modal-trigger').leanModal();
+    
+    /////////////////////////////////////////
+    // Materialize Color change selection
+    /////////////////////////////////////////
+    var currentColor = $('#change-color').val();
+    $('#change-color').on('change', function(){
+        // Give me the new color
+        var newColor = $(this).val();
+        
+        // Select the class of currentColor, replace with newColor
+        $('.' + currentColor + '').addClass(newColor)
+            .removeClass(currentColor);
+        
+        // Reset current color
+        currentColor = newColor;
+    });
+    
 });
